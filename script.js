7c39a49bf59d660b7c9c5a694df8f2d12e0191be
leticiadocrime <106694092+leticiadocrime@users.noreply.github.com>
2024-06-12 15:35:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Qual estilo vocẽ prefere?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "afirmação"
+                texto: "Casual",
+                afirmacao: "Elegante"
             },
             {
                 texto: "Isso é maravilhoso!",
