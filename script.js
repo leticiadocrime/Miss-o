d4c29b9535eed225d33404b3c3edb78b6cc739d7
leticiadocrime <106694092+leticiadocrime@users.noreply.github.com>
2024-06-12 14:40:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -54,8 +54,12 @@ function mostraPergunta() {
 function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
-        caixaAlternativas.appendChild(botaoAlternativas);
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", function () {
+          atual++;
+          mostraPergunta();
+        })
+      caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
 
